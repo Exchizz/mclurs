a90c5b365f699f89736224de48b4fdc1f591c451
John Hallam <sw@j.hallam.dk>
2016-01-06 16:31:08 +0100

    Finished I2C probe code in mclurs-probe

--- array-tools-1.0/hardware/mclurs-probe.sh
+++ array-tools-1.0/hardware/mclurs-probe.sh
@@ -11,8 +11,9 @@ I2CBUS=''
 for BUS in `i2cdetect -l | awk '{print substr($1,5); }'`; do
     echo -n Probing using I2C Bus $BUS ...
     if i2cdetect -y $BUS > /tmp/i2c-$BUS-detect; then
-	if grep -q 18 /tmp/i2c-$BUS-detect && grep -q 41 /tmp/i2c-$BUS-detect; then
+	if grep -q ' 18 ' /tmp/i2c-$BUS-detect && grep -q ' 41 ' /tmp/i2c-$BUS-detect; then
 	    I2CBUS=$BUS
+	    echo " accepted $BUS"
 	    break
 	fi
     else
@@ -26,6 +27,25 @@ if [ "x$I2CBUS" = "x" ]; then
     exit 1
 fi
 
+Version=0
+echo DS2482  found on I2C Bus $I2CBUS
+echo LTC2637 found on I2C Bus $I2CBUS
 
+if grep -q ' 6f ' /tmp/i2c-$I2CBUS-detect && grep -q ' 57 ' /tmp/i2c-$I2CBUS-detect; then
+    echo MCP79410 found on I2C Bus $I2CBUS
+    Version=1
+fi
+
+if grep -q ' 50 ' /tmp/i2c-$I2CBUS-detect; then
+    echo Found 24AA64 on I2C Bus $I2CBUS
+fi
+
+if grep -q ' 27 ' /tmp/i2c-$I2CBUS-detect || grep -q ' UU ' /tmp/i2c-$I2CBUS-detect; then
+    echo Found PCA9534 on I2C Bus $I2CBUS
+    Version=2
+fi
+
+echo I2C Probe complete, Hardware Version $Version
+rm -f /tmp/i2c-$I2CBUS-detect
 
 exit 0
