892a68ca362acf8d1a0e8cce46fbc1d379fb9031
John Hallam <sw@j.hallam.dk>
2016-01-06 16:06:49 +0100

    Added I2C probe step 1 to mclurs-probe.sh

--- array-tools-1.0/hardware/mclurs-probe.sh
+++ array-tools-1.0/hardware/mclurs-probe.sh
@@ -5,6 +5,10 @@ echo MCLURS Hardware Probe Vn 0.5
 NCPU=`grep processor /proc/cpuinfo | wc -l`
 UUIDPI=`grep serial /proc/cpuinfo | awk '{print $3;}'`
 
-echo Detected a ${NCPU}cpu Pi with serial number $UUIDPI
+echo Detected a ${NCPU}-cpu Pi with serial number $UUIDPI
+
+I2CBUS=`i2cdetect -l | awk '{print substr($1,4); }'`
+
+echo Using I2C Bus $I2CBUS ...
 
 exit 0
