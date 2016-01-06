49adae93b406aa66c675561877648c113c622081
John Hallam <sw@j.hallam.dk>
2016-01-06 16:20:13 +0100

    Added I2C probe code to mclurs-probe

--- array-tools-1.0/hardware/mclurs-probe.sh
+++ array-tools-1.0/hardware/mclurs-probe.sh
@@ -7,8 +7,25 @@ UUIDPI=`grep serial /proc/cpuinfo | awk '{print $3;}'`
 
 echo Detected a ${NCPU}-cpu Pi with serial number $UUIDPI
 
-I2CBUS=`i2cdetect -l | awk '{print substr($1,4); }'`
+I2CBUS=''
+for BUS in `i2cdetect -l | awk '{print substr($1,5); }'`; do
+    echo -n Probing using I2C Bus $BUS ...
+    if i2cdetect -y $BUS > /tmp/i2c-$BUS-detect; then
+	if grep -q 18 /tmp/i2c-$BUS-detect && grep -q 41 /tmp/i2c-$BUS-detect; then
+	    I2CBUS=$BUS
+	    break
+	fi
+    else
+	echo " failed"
+	rm -f /tmp/i2c-$BUS-detect
+    fi
+done
 
-echo Using I2C Bus $I2CBUS ...
+if [ "x$I2CBUS" = "x" ]; then
+    echo Unable to determine I2C Bus to talk to hardware
+    exit 1
+fi
+
+
 
 exit 0
